8c06c4e3e1380585956466881b5149e6a5687283
Kenny-Nuel <kenny.ozigbo@gmail.com>
2024-04-07 00:45:46 +0000

    Week 4 Practices

---
+++ Week 4/exercise-3.py
@@ -0,0 +1,18 @@
+# COUPE DE ESCRIVA 2023: FOOTBALL PICS
+
+print("Welcome to the COUPE DE ESCRIVA 2023: FOOTBALL PICS \n")
+
+captain = {'Madiba': 'Chubby Obiara-Okafo', 'Blue-Jays: ': 'Christopher Umeh', 'Cirok': 'Alexander', 'TSG Walkers: ': 'Ikechukwu'}
+
+goalkeepers = {'Madiba': 'Chubby Obiara-Okafo', 
+'Blue-Jays': 'Oladimeji Abaniwondea / Jeffery Awagu',
+'Cirok: ': 'Timileyin Pearse / Izuako Jeremy',
+'TSG Walkers': 'Ayomide Ojituku'}
+
+for pick in captain:
+	print(pick, captain[pick])
+
+print("\n")
+
+for pick in goalkeepers:
+	print(pick, goalkeepers[pick])
